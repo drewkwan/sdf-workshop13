37f133c9349a2f93ec0ba5cc8c0c4d6a304b11ae
drewkwan <andrewtimkwan@gmail.com>
2022-07-14 13:54:30 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+## Workshop 13
+
+* Run the spring boot app
+
+Linux/Macos
+'''
+mvn spring-boot:run -Dspring-boot:run.arguments=--dataDir=/opt/tmp/data
+'''
+mv spring-boot: run -Dspring-boot:run.arguments=--dataDir=C:/data
+'''
+* Incorporate test started dependencies
+'''
+'''
+<dependency>
+    <groupId>org.springframework.boot</groupId>
+    <artifactId>spring-boot-starter-test</artifactId>
+    <version>2.7.1</version>
+    <scope>test</scope>
+</dependency>
+'''
+
+* In order to execute test cases
+'''
+mvn test
+'''
+
+'''
---
+++ src/main/java/vttp2022b2/workshop13/workshop13/model/Contact.java
@@ -0,0 +1,68 @@
+package vttp2022b2.workshop13.workshop13.model;
+
+import java.io.Serializable;
+import java.util.Random;
+
+public class Contact implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private String name;
+    private String email;
+    private int phoneNumber;
+    private String id;
+
+    public Contact() {
+        this.id = generateID(0);
+    }
+
+    public Contact(String name, String email, int phoneNumber) {
+        this.id = generateID(0);
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Contact(String id, String name, String email, int phoneNumber) {
+        this.id = generateID(0);
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    } //Overloading, this is for "future use" and isn't relevant to this workshop necessarily
+
+    private synchronized String generateID(int numChars) {    //what is synchronized keyword in java?  //what is a race condition? 
+        Random r = new Random();
+        StringBuilder sb = new StringBuilder();
+        while (sb.length() < numChars) {
+            sb.append(Integer.toHexString(r.nextInt()));
+        }
+
+        return sb.toString().substring(0,numChars);
+
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+}
